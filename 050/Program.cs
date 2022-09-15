27db1510845aec62a08274545a685b4842221fdf
Hanna Pon <hannainleto@mail.ru>
2022-09-15 13:34:49 +0300

    Задача 50

--- 050/Program.cs
+++ 050/Program.cs
@@ -1,67 +1,51 @@
-﻿// Задача 50: Напишите программу, которая на вход принимает 
-// позиции элемента в двумерном массиве, и возвращает значение 
-// этого элемента или же указание, что такого элемента нет.
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-Console.Write("\nMассив возьмем из предыдущей задачи (№ 47).\n");
-Console.Write("Введите координаты позиции элемента, разделенных запятой: ");
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[6, 8];
+FillArray2D(numbers);
+PrintArray2D(numbers);
 
-string? positionElement = Console.ReadLine();
-positionElement = RemovingSpaces(positionElement);
-int[] position = ParserString(positionElement);
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
 
-if (position[0] && position[1]  
-&& position[0] >= 0 
-&& position[1] >= 0) 
+
+// Заполнение массива рандомными числами от 1 до 9
+void FillArray2D(int[,] array)
 {
-  double result = array[position[0]-1, position[1]-1];
-  Console.Write($"Значение элемента: {result}");
-}
-else Console.Write($"такого элемента в массиве нет.");
-
-int[] ParserString(string input)
-{
-  int countNumbers = 1;
-  for (int i = 0; i < input.Length; i++)
-  {
-      if (input[i] == ',')
-          countNumbers++;
-  }
-
-  int[] numbers = new int[countNumbers];
-
-  int numberIndex = 0;
-  for(int i = 0; i < input.Length; i++)
-  {
-    string subString = String.Empty;
-
-    while (input[i] != ',')
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      subString += input[i].ToString();
-      if (i >= input.Length - 1)
-        break;
-      i++;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
     }
-    numbers[numberIndex] = Convert.ToInt32(subString);
-    numberIndex++;
-  }
-
-  return numbers;
 }
 
-string RemovingSpaces (string input)
+//  Функция вывода массива в терминал
+void PrintArray2D(int[,] array)
 {
-  string output = String.Empty;
-  for (int i = 0; i < input.Length; i++)
-  {
-    if (input[i] != ' ') 
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      output += input[i];
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
     }
-  }
-  return output;
+    Console.WriteLine();
+}
+
+// Функция ввода 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
